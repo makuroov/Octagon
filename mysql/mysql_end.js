d6262f58fe4e8410a5f88aeb40467911cf81abc2
Владимир <151106907+makuroov@users.noreply.github.com>
2023-12-11 23:30:35 +0800

    Add files via upload

---
+++ mysql/mysql_end.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2");
+  
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "chatbottests",
+  password: ""
+});
+
+connection.end(function(err) {
+    if (err) {
+      return console.log("Ошибка: " + err.message);
+    }
+    console.log("Подключение закрыто");
+  });
