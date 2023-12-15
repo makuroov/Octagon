0b197110fa1c4e04d57b9f4c32097d16575c76b8
Владимир <151106907+makuroov@users.noreply.github.com>
2023-12-16 01:45:06 +0800

    Delete bot.js

--- bot.js
+++
@@ -1,9 +0,0 @@
-const TelegramBot = require('node-telegram-bot-api');
-const token = '6970104134:AAFafIkauKbTLpjCfI8KB7qqVxxL5LbrGCg';
-const bot = new TelegramBot(token, {polling: true});
-
-bot.on('message', (msg) => 
-{
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Привет, Октагон!');
-});
